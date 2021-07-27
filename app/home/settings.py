2fff77dd0e929d1fca10830a5dd6d2c31d998f31
brandon <brndnlstr@gmail.com>
2021-07-26 20:33:39 -0400

    disabled debug mode

--- app/home/settings.py
+++ app/home/settings.py
@@ -29,8 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = Secrets.SECRET_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+#DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ['*']
 # ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
 
