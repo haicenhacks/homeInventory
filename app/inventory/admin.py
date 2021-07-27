7b5f34811fb72c8c311c6556aab08245871b4771
brandon <brndnlstr@gmail.com>
2021-07-27 14:01:33 -0400

    minor change to settings file, registered user in admin

--- app/inventory/admin.py
+++ app/inventory/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Room, Storage, Item, Document
+from .models import Room, Storage, Item, Document, CustomUser
 
 
 class DocumentInlineAdmin(admin.StackedInline):
@@ -32,7 +32,7 @@ admin.site.register(Room)
 admin.site.register(Storage, StorageAdmin)
 admin.site.register(Item, ItemAdmin)
 admin.site.register(Document, DocumentAdmin)
-
+admin.site.register(CustomUser)
 admin.site.site_title = "Home Inventory Admin"
 admin.site.site_header = "Home Inventory"
 admin.site.index_title = "Site Settings"
